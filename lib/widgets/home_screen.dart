aef4282ab54989fbfc6d36ea5de2adb03998d089
Drew <chukwukaenudeme@gmail.com>
2026-06-09 19:26:31 +0100

    feat: Add Windows support for Flutter application
    - Created CMakeLists.txt for Windows build configuration.
    - Added manifest.json for web application settings.
    - Implemented Windows runner files including main application logic, window management, and resource handling.
    - Introduced utility functions for console management and command line argument parsing.
    - Added resource files for application icon and versioning.
    - Configured plugin registration and build rules for Flutter plugins.
    - Established project structure for Windows-specific code and resources.

---
+++ lib/widgets/home_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  void _changeIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  final chart = [
+    {
+      "name": "Jennifer",
+      "message": "How far?",
+      "time": "11:49 AM",
+      "image":
+          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=200",
+    },
+    {
+      "name": "Software Engineers",
+      "message": "Harry: And the words of the HOD...",
+      "time": "8:24 AM",
+      "image":
+          "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=200",
+    },
+    {
+      "name": "Future Software Engineers",
+      "message": "Harry added +234 701 733 5496",
+      "time": "Yesterday",
+      "image":
+          "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?w=200",
+    },
+    {
+      "name": "mark",
+      "message": "How far",
+      "time": "11:49 AM",
+      "image":
+          "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=200",
+    },
+    {
+      "name": "Prevail",
+      "message": "Voice message (0:15)",
+      "time": "10:02 AM",
+      "image":
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    },
+    {
+      "name": "Prevail",
+      "message": "Voice message (0:15)",
+      "time": "10:02 AM",
+      "image":
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    },
+    {
+      "name": "Prevail",
+      "message": "Voice message (0:15)",
+      "time": "10:02 AM",
+      "image":
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    },
+    {
+      "name": "Prevail",
+      "message": "Voice message (0:15)",
+      "time": "10:02 AM",
+      "image":
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    },
+    {
+      "name": "Prevail",
+      "message": "Voice message (0:15)",
+      "time": "10:02 AM",
+      "image":
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xff25D366),
+        onPressed: () {},
+        child: HugeIcon(
+          icon: HugeIcons.strokeRoundedBubbleChatAdd,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(top: 20),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _changeIndex,
+          selectedItemColor: Color(0xff25D366),
+          unselectedItemColor: Colors.black87,
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              label: "Chats",
+              icon: HugeIcon(icon: HugeIcons.strokeRoundedMessage01),
+            ),
+            BottomNavigationBarItem(
+              label: "Updates",
+              icon: HugeIcon(icon: HugeIcons.strokeRoundedSolarSystem01),
+            ),
+            BottomNavigationBarItem(
+              label: "Communities",
+              icon: HugeIcon(icon: HugeIcons.strokeRoundedUserGroup),
+            ),
+            BottomNavigationBarItem(
+              label: "Calls",
+              icon: HugeIcon(icon: HugeIcons.strokeRoundedCall),
+            ),
+          ],
+        ),
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "WhatsApp",
+                    style: TextStyle(
+                      color: Color(0xff25D366),
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: const [
+                      HugeIcon(icon: HugeIcons.strokeRoundedCamera01),
+                      SizedBox(width: 10),
+                      HugeIcon(icon: HugeIcons.strokeRoundedMoreVertical),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
