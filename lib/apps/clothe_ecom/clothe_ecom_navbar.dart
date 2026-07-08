59877e18da5a2a6d88ab54bcd06f20d389d4f884
Drew <chukwukaenudeme@gmail.com>
2026-07-08 13:07:30 +0100

    feat: Add ClotheEcomNavbar component with basic structure

---
+++ lib/apps/clothe_ecom/clothe_ecom_navbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClotheEcomNavbar extends StatelessWidget {
+  const ClotheEcomNavbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
