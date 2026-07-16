28b9d1103e7a2d5454fc492a124fec19aa722f9b
Drew <chukwukaenudeme@gmail.com>
2026-07-16 11:38:57 +0100

    fix: Correct typo in button text from "See  All" to "See  all"

--- lib/apps/cloth_ecom/utils/cloth_ecom_products.dart
+++ lib/apps/cloth_ecom/utils/cloth_ecom_products.dart
@@ -160,7 +160,7 @@ class _ClothEcomProductsState extends State<ClothEcomProducts> {
                 TextButton(
                   onPressed: () {},
                   child: Text(
-                    "See  All",
+                    "See  all",
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
                       fontSize: 13,
